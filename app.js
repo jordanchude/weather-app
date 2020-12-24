4035e9af547820db2927184e4d0fc394725e93ad
JordanC <jcwilliams410@gmail.com>
2020-12-23 19:58:11 -0500

    postman request module added

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-console.log('Starting');
-
-// run function after two seconds has passed
-setTimeout(() => {  
-    console.log('2 second timer');
-}, 2000)
-
-console.log('Stopping');
+// DEPENDENCIES
+const request = require('postman-request');
+const url = 'http://api.weatherstack.com/current?access_key=76d4262f3cad2ae3626ee18201185b27&query=37.8267,-122.4233'
