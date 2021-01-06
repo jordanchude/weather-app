44df915c332c9db0ccf2d6d499f109fd0ded82cc
JordanC <jcwilliams410@gmail.com>
2021-01-06 09:20:22 -0500

    forecast function addition

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const request = require('postman-request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const url = 'http://api.weatherstack.com/current?access_key=76d4262f3cad2ae3626ee18201185b27&query=37.8267,-122.4233&units=f'
 
@@ -39,7 +40,22 @@ const url = 'http://api.weatherstack.com/current?access_key=76d4262f3cad2ae3626e
 //     }
 // });
 
-geocode('Philadelphia New York', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+// geocode('Philadelphia New York', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(37.8267, -122.4233, (error, data) => {
+    console.log('Error:', error)
+    console.log('Data:', data)
+  })
---
+++ utils/forecast.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    // url to send request to
+    const url = `http://api.weatherstack.com/current?access_key=76d4262f3cad2ae3626ee18201185b27&query=${latitude},${longitude}&units=f`
+
+    request ({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined)
+        } else if (response.body.success === false) {
+            callback('Unable to find coordinates', undefined)
+        } else {
+            callback(undefined, `${response.body.location.name}. It is currently ${response.body.current.temperature}° degrees out and ${response.body.current.weather_descriptions}. It feels like ${response.body.current.feelslike} °degrees out.`)
+        }
+    })
+}
+
+module.exports = forecast
